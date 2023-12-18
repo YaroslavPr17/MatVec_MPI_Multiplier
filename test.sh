7758f30668a943b944b3ef2e9d744a52a5c03c99
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2023-12-18 19:23:08 +0300

    experimental: Add metrics for symmetric matrices

--- test.sh
+++ test.sh
@@ -2,18 +2,12 @@
 
 TYPE=$1
 
-N_ROWS_START_VALUE=120
-N_ROWS_FINISH_VALUE=1201
-N_ROWS_STEP=120
-
-N_COLS=60000
-
 for n_proc in 1 2 6 12 24
 do
     echo $n_proc
-    for ((n_rows=$N_ROWS_START_VALUE; n_rows < $N_ROWS_FINISH_VALUE; n_rows += $N_ROWS_STEP))
+    for n_rows in 600 1800 3000 4200 5400 6600 7800 9000 10200
     do
         mpicc ./src/multiplier_$TYPE.c ./src/matr_utils.c ./src/utils.c -o ./out/multiplier -Wall -lm
-        mpiexec -n $n_proc ./out/multiplier $n_rows $N_COLS
+        mpiexec -n $n_proc ./out/multiplier $n_rows $n_rows
     done
 done
